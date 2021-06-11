3738a523b27cc65d58d1f1519963abfbf9c84d0f
christiangerbig <chr_gerbig@web.de>
2021-06-11 17:59:12 +0200

    destroy image added 3

--- routes/cloudinary.routes.js
+++ routes/cloudinary.routes.js
@@ -25,8 +25,8 @@ router.post(
 router.post(
   "/destroy",
     (req, res, next) => {
-    let imageId = req.body.imageId;
-    uploader.destroy(imageId)
+    // let imageId = req.body.imageId;
+    uploader.destroy("jnu2y1o9hyn0v1ohxfp3")
       .then(
         (response) => {
           res.status(200).json(response);
