469b8bbf750f45ad47f96c29cdd03b8db3643f6b
christiangerbig <chr_gerbig@web.de>
2021-06-24 15:43:32 +0200

    messages routes 1.1

--- routes/messages.routes.js
+++ routes/messages.routes.js
@@ -2,35 +2,9 @@ const express = require("express");
 const router = express.Router();
 const RequestModel = require("../models/Request.model");
 
-// POST Request
-router.post(
-  "/plants/request",
-  (req, res) => {
-    const { message, buyer, seller, plant } = req.body;
-    const request = {
-      buyer,
-      seller,
-      plant,
-      message
-    }
-    RequestModel.create(request)
-      .then(
-        (response) => res.status(200).json(response)
-      )
-      .catch(
-        (err) => res.status(500).json(
-          {
-            error: "Create request failed",
-            message: err
-          }
-        )
-      );
-  }
-);
-
-// GET My requests
+// GET Requests
 router.get(
-  "/myrequests",
+  "/requests",
   (req, res) => {
     RequestModel.find({})
       .then(
@@ -43,6 +17,32 @@ router.get(
         (err) => res.status(500).json(
           {
             error: "Get requests failed",
+            message: err
+          }
+        )
+      );
+  }
+);
+
+// POST Create request
+router.post(
+  "/requests/create",
+  (req, res) => {
+    const { message, buyer, seller, plant } = req.body;
+    const newRequest = {
+      buyer,
+      seller,
+      plant,
+      message
+    }
+    RequestModel.create(newRequest)
+      .then(
+        (response) => res.status(200).json(response)
+      )
+      .catch(
+        (err) => res.status(500).json(
+          {
+            error: "Create request failed",
             message: err
           }
         )
