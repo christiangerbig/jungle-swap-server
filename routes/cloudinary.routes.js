00cd6e435e055d04d6955805acbd92dbea3215d7
christiangerbig <chr_gerbig@web.de>
2021-07-04 23:14:13 +0200

    destroy image added

--- routes/cloudinary.routes.js
+++ routes/cloudinary.routes.js
@@ -2,15 +2,34 @@ const express = require("express");
 const router  = express.Router();
 const uploader = require("../config/cloudinary.config.js");
 
+// Upload image
 router.post(
   "/upload", 
   uploader.single("image"), 
   (req, res) => {
-    if (!req.file) {
-      res.status(500).json({ error: "Please choose an image" });
-      return;
-    }
+    if (!req.file) return res.status(500).json({ error: "Please choose an image" })
     res.status(200).json({ image: req.file.path });
+  }
+);
+
+// Delete image
+router.post(
+  "/destroy",
+  (req, res) => {
+    try {
+      const { publicId } = req.body;
+      if (!publicId) return res.status(400).json({ error: "No image chosen" })
+      uploader.destroy(
+        publicId, 
+        async(err, result) => {
+          if(err) throw err;
+          res.json({ msg: "Image deleted" });
+        }
+      );
+    } 
+    catch (err) {
+      return res.status(500).json({ error: err.message });
+    }
   }
 );
 
