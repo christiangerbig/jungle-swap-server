1ad65d20dcf7bdfb6d1f1d1f0509452f35eb666e
christiangerbig <chr_gerbig@web.de>
2021-07-05 11:55:23 +0200

    publicId added 1.6

--- routes/cloudinary.routes.js
+++ routes/cloudinary.routes.js
@@ -21,21 +21,22 @@ router.post(
 router.post(
   "/destroy",
   (req, res) => {
-    try {
-      const { public_id } = req.body;
-      if (!public_id) return res.status(400).json({ error: "No image chosen" })
-      uploader.destroy(
+    //try {
+      //const { public_id } = req.body;
+      //if (!public_id) return res.status(400).json({ error: "No image chosen" })
+      const public_id = g0nmildklkor3gjljnws;
+      cloudinary.uploader.destroy(
         public_id, 
         /*async(err, result) => {
           if(err) throw err;
           res.json({ msg: "Image deleted" });
         }*/
-        result => { return result}
+        result => { return result;}
       );
-    } 
+    /*} 
     catch (err) {
       return res.status(500).json({ error: err.message });
-    }
+    }*/
   }
 );
 
