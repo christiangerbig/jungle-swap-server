8bd27108198894fa71d0e82789d74024e7759434
christiangerbig <chr_gerbig@web.de>
2021-11-23 20:52:30 +0100

    endpoint destroy changed

--- routes/cloudinary.routes.js
+++ routes/cloudinary.routes.js
@@ -18,7 +18,14 @@ router.post("/cloudinary/upload", uploader.single("image"), (req, res) => {
 // Delete image
 router.post("/cloudinary/destroy", (req, res) => {
   const { imagePublicId } = req.body;
-  cloudinary.uploader.destroy(imagePublicId, (result) => {
+  if (!imagePublicId) {
+    res.status(400).json({ error: "No image selected" });
+    return;
+  }
+  cloudinary.uploader.destroy(imagePublicId, (err, result) => {
+    if (err) {
+      throw err;
+    }
     res.status(200).json(result);
   });
 });
